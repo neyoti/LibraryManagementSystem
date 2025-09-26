1e8cd4393b8d9a985e52ccf1b27c1db192d1dfe3
neyoti <amitcs196@gmail.com>
2025-09-27 00:18:22 +0530

    Added models and dbcontext

---
+++ LibraryManagementSystem/LibraryManagementSystem/Models/BorrowedRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LibraryManagementSystem.Models
+{
+    public class BorrowedRecord
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book? Book { get; set; }
+        public int MemberId { get; set; }
+        public Member? Member { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
+
