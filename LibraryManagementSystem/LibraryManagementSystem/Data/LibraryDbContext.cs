7a15e70456234409221c0acc36ea9f77fb586446
neyoti <amitcs196@gmail.com>
2025-09-27 00:37:36 +0530

    Added Models and DbContext

---
+++ LibraryManagementSystem/LibraryManagementSystem/Data/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data
+{
+	public class LibraryDbContext : DbContext
+	{
+		public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+
+		DbSet<Book> Books { get; set; }
+		DbSet<Member> Members { get; set; }
+		DbSet<BorrowedRecord> BorrowedRecords { get; set; }
+	}
+}
+
